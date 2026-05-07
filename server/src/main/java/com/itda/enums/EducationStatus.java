e1a99d2872d4d7a739bb1da6236384d2b591e2f5
dlwlsqor10 <dlwlsqor10@naver.com>
2026-05-07 19:15:46 +0900

    feet : 학력/재학상태/성별 ENUM

---
+++ server/src/main/java/com/itda/enums/EducationStatus.java
@@ -0,0 +1,10 @@
+package com.itda.enums;
+
+// 재학 상태
+public enum EducationStatus {
+    ENROLLED,       // 재학중
+    GRADUATED,      // 졸업
+    EXPECTED,       // 졸업예정
+    DROPPED,        // 중퇴
+    LEAVE           // 휴학중
+}
---
+++ server/src/main/java/com/itda/enums/Gender.java
@@ -0,0 +1,6 @@
+package com.itda.enums;
+
+public enum Gender {
+    MALE,   // 남성
+    FEMALE  // 여성
+}
