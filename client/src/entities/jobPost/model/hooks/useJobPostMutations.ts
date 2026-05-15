2129946f9888cfbd983a6be2bd6eec6592aa16ce
KMU-jeonghj <hjjung70@kookmin.ac.kr>
2026-05-15 11:52:57 +0900

    Fix: 클라이언트 빌드 에러 수정 - 미사용 import 제거

--- client/src/entities/jobPost/model/hooks/useJobPostMutations.ts
+++ client/src/entities/jobPost/model/hooks/useJobPostMutations.ts
@@ -1,5 +1,4 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
 import { closeJobPost } from 'entities/jobPost/api/jobPost.api';
 
 /**
